0f032191615bcc0ea5f2282ea1fd64f26aae9287
abhishek-kumaraj <abhishekkumaraj1506@gmail.com>
2023-08-12 02:05:20 +0530

    added validation for task model

--- models/Tasks.js
+++ models/Tasks.js
@@ -1,8 +1,16 @@
 const mongoos = require('mongoose')
 
 const taskSchema = new mongoos.Schema({
-    name:String,
-    completed:Boolean
+    name:{
+        type:String,
+        required: true,
+        trim: true,
+        maxlength:20
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }
 })
 
 module.exports = mongoos.model('Task',taskSchema)
