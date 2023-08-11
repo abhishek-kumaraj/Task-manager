444b6891b3cc83874dc26d73495f231f9e50bdd2
abhishek-kumaraj <abhishekkumaraj1506@gmail.com>
2023-08-12 01:48:30 +0530

    commented some code

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const app = express()
 const task = require('./routes/tasks')
 
 const mongoDB = require('./db/connection')
-require('dotenv').config()
-app.use(express.json())
+require('dotenv').config() // loads the environment variable defined in .env file into process.env object
+app.use(express.json()) // build in middleware funciton to parse incoming JSON data form the request body
 app.use(express.static('./public'))
-app.get('/hello',(req,res)=>{
-    res.send('Hello there')
-})
+// app.get('/hello',(req,res)=>{
+//     res.send('Hello there')
+// })
 
 // app.get('/api/v1/tasks') - get all the tasks
 // app.post('/api/v1/tasks') - create a new task
-app.use('/api/v1/tasks',task)
+app.use('/api/v1/tasks',task) // mount the router on the app
 
 
 // app.get('/api/v1/tasks/:id') - get info about a task
